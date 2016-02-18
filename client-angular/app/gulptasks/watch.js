578680474a40a387daad05678c1ea4da7cae1cff
Vivian Mathews <jmathews@macadamian.com>
2016-02-18 09:55:51 -0500

    These guys tried to escape?!!?!
    Change-Id: I797c005be2c0368b377ae27298cf0af8e441740e

--- client-angular/app/gulptasks/watch.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-
-gulp.task('watch', ['jshintAndJscs'], function() {
-  gulp.watch(['./**/*.scss'], ['sass']);
-  gulp.watch(['./**/*.js'], ['jshintAndJscs']);
-  gulp.watch(['./*.html', './**/*.html', './**/*.js'], ['browser-sync-reload']);
-});
