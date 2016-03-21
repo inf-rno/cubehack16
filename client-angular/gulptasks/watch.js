27fe2ff95ad1f6a5d460ca8ee8c23c047684bd2a
Christian Nadeau <cnadeau@macadamian.com>
2016-03-21 10:12:54 -0400

    Added jshintAndJscs to tasks triggered when code is modified
    Change-Id: Ic1804fe0d70ac25f1be757ea24dcd47cb21e87e1

--- client-angular/gulptasks/watch.js
+++ client-angular/gulptasks/watch.js
@@ -4,5 +4,6 @@ var gulp = require('gulp');
 
 gulp.task('watch', function() {
   gulp.watch(['./app/**/*.scss'], ['sass']);
-  gulp.watch(['./app/*.html', './app/**/*.html', './app/**/*.js', '!./app/index.html'], ['browser-sync-reload']);
+  gulp.watch(['./app/*.html', './app/**/*.html', './app/**/*.js', '!./app/index.html'], 
+  	['jshintAndJscs', 'browser-sync-reload']);
 });
