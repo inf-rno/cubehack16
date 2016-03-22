1d49ca522efddb84ff61a2751a8893357f12e7fb
Christian Nadeau <cnadeau@macadamian.com>
2016-03-22 10:03:52 -0400

    Properly handle sass error to prevent breaking the build
    Change-Id: If6cd73523bd332ec4050af71370787d1f29cec76

--- client-angular/gulptasks/sass.js
+++ client-angular/gulptasks/sass.js
@@ -9,7 +9,7 @@ var loadPlugins = require('gulp-load-plugins')({
 
 gulp.task('sass', function() {
   return gulp.src(['./app/assets/styles/style.scss'])
-    .pipe(loadPlugins.sass())
+    .pipe(loadPlugins.sass().on('error', loadPlugins.sass.logError))
     .pipe(loadPlugins.autoprefixer({
       browsers: ['last 2 versions'],
       cascade: true
