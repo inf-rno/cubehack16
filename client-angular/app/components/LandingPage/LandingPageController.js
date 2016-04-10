209e32392599692286c7722d058ba03b7649a75e
Jake Tober <jake@macadamian.com>
2016-04-09 20:27:54 -0400

    Client shows basic game states

--- client-angular/app/components/LandingPage/LandingPageController.js
+++ client-angular/app/components/LandingPage/LandingPageController.js
@@ -9,7 +9,54 @@
 
   function LandingPageController($scope, url, $http) {
     var vm = this;
-
+    vm.state = "";
     vm.requestOutput = url;
+    vm.color = "";
+    vm.playerName = "";
+    vm.winner = "";
+    vm.didRegister = false;
+    vm.didWin = false;
+
+    function handleGameUpdate(data)
+    {
+        vm.state = data.state;
+        vm.didWin = data.winner === vm.color;
+    }
+    
+    var requestGameUpdate = _.throttle(function()
+    {
+        $http.get(url+"game/1").then(function(response)
+        {
+            if(response.status===200)
+            {
+                handleGameUpdate(response.data);
+            }
+            else
+            {
+                requestGameUpdate();
+            }
+        }, requestGameUpdate);
+    },2000);
+    
+    vm.start = function()
+    {
+        console.log(vm.color);
+        vm.state = "inProgress";
+        $http.put(url+"game/1", {state:"inProgress"}).then(requestGameUpdate);
+    }
+    vm.reset = function()
+    {
+        vm.state = "init";
+        vm.didRegister = false;
+        $http.put(url+"game/1", {state:"init"}).then(requestGameUpdate);
+    }
+    vm.register = function()
+    {
+        if(vm.didRegister){return;}
+        vm.didRegister = true;
+        $http.post(url+"game/1/players", {name:vm.playerName, color:vm.color}).then(requestGameUpdate);
+    }
+    
+    setInterval(requestGameUpdate, 2000);
   }
 })();
